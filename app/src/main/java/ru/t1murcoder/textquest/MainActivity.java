dd6fa7cb9b635ed543a18a6ad895a5218c46e286
T1murCoder <timur.zarutskiy@yandex.ru>
2023-12-19 19:27:54 +0300

    Initial commit

---
+++ app/src/main/java/ru/t1murcoder/textquest/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.t1murcoder.textquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
